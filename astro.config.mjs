975c8d713c59fe6d742133eed6b39ba3d6cf24a6
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2025-12-30 21:21:29 -0500

    Configure GitHub Pages deployment with GitHub Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://farzammohammadi.github.io',
+  base: '/algo-theater',
   output: 'static',
   vite: {
     css: {
